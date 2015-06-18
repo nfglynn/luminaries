7543cbf9b3f5e38a25ae1309e23d1c0b45b6428b
Niall Glynn <nglynn@gmail.com>
2015-06-18 16:36:04 +0100

    All working and printing character report.

--- luminaries.py
+++ luminaries.py
@@ -1,44 +1,99 @@
 import datetime
-import astral
 import ephem
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 Location = namedtuple('Location',
-                      ['astral_name',
-                       'lat',
+                      ['lat',
                        'lon'])
 
-LUMINARIES_LOCATION = Location(astral_name='Wellington',
-                               lat=-45.8667,
+LUMINARIES_LOCATION = Location(lat=-45.8667,
                                lon=170.5000)
+
+
+Constellation = namedtuple('Constellation',
+                           ['symbol',
+                            'element',
+                            'character'])
+
+
+class Constellation():
+    ZODIAC = {'Aries': ('♈', 'Fire', 'Te Rau Tauwhare'),
+              'Taurus': ('♉', 'Earth', 'Charlie Frost'),
+              'Gemini': ('♊', 'Air', 'Benjamin Lowenthal'),
+              'Cancer': ('♋', 'Water', 'Edgar Clinch'),
+              'Leo': ('♌', 'Fire', 'Dick Mannering'),
+              'Virgo': ('♍', 'Earth', 'Quee Long'),
+              'Libra': ('♎', 'Air', 'Harald Nilssen'),
+              'Scorpius': ('♏', 'Water', 'Joseph Pritchar'),
+              'Sagittarius': ('♐', 'Fire', 'Thomas Balfour'),
+              'Capricornus': ('♑', 'Earth', 'Aubert Gascoigne'),
+              'Aquarius': ('♒', 'Air', 'Sook Yongsheng'),
+              'Pisces': ('♓', 'Water', 'Cowell Devlin')}
+
+    CONSTELLATION_MAPPING = {'Ophiuchus': 'Sagittarius',
+                             'Sextans': 'Leo'}
+
+    @classmethod
+    def from_name(cls, name):
+        constellation_name = cls.CONSTELLATION_MAPPING.get(name, name)
+        symbol, element, character = cls.ZODIAC[constellation_name]
+        return cls(constellation_name, symbol, element, character)
+
+    def __init__(self, name, symbol, element, character):
+        self.name = name
+        self.symbol = symbol
+        self.element = element
+        self.character = character
+
+    def __str__(self):
+        return self.name
+
+
+class Planet():
+    PLANET_CHARACTERS = OrderedDict(Mercury='Walter Moody',
+                                    Venus='Lydia Carver',
+                                    Mars='Francis Carver',
+                                    Jupiter='Alistair Lauderback',
+                                    Saturn='George Shepard',
+                                    Moon='Anna Wetherell',
+                                    Sun='Emery Staines')
+
+    @classmethod
+    def from_name_and_date(cls, name, date):
+        constructor = getattr(ephem, name)
+        planet = constructor()
+        planet.compute(date)
+        return cls(planet, cls.PLANET_CHARACTERS[name])
+
+    def __init__(self, ephem_planet, character):
+        self._ephem_planet = ephem_planet
+        self.character = character
+
+    @property
+    def name(self):
+        return self._ephem_planet.name
+
+    @property
+    def mag(self):
+        return self._ephem_planet.mag
+
+    @property
+    def constellation(self):
+        return Constellation.from_name(ephem.constellation(self._ephem_planet)[1])
+
+    @property
+    def phase(self):
+        return int(self._ephem_planet.phase)
 
 
 class Sky():
     def __init__(self, date):
         self.date = date
-        self._astral = astral.Astral()[LUMINARIES_LOCATION.astral_name]
         self._ephem = ephem.Observer()
-        self._ephem.time = date,
-        self._ephem.lat = LUMINARIES_LOCATION.lat,
+        self._ephem.date = "{}/{}/{}".format(date.year, date.month, date.day)
+        self._ephem.lat = LUMINARIES_LOCATION.lat
         self._ephem.lon = LUMINARIES_LOCATION.lon
-
-
-    @property
-    def moon_phase(self):
-        return self._astral.moon_phase(self.date)
-
-    @property
-    def mars_mag(self):
-        
-        pass
-
-    @property
-    def jupiter_mag(self):
-        pass
-
-    @property
-    def saturn_mag(self):
-        pass
+        self.planets = [Planet.from_name_and_date(name, self._ephem.date) for name in Planet.PLANET_CHARACTERS]
 
 
 class Part():
@@ -47,6 +102,19 @@ class Part():
         self.title = title
         self.date = date
         self.sky = Sky(self.date)
+
+    def __str__(self):
+        s = 'Part {}: "{}", {}\n'.format(self.number, self.title, self.date)
+        for planet in self.sky.planets:
+            s += '\t{}: ({}%, {}) in {}\n'.format(planet.name,
+                                                  planet.phase,
+                                                  planet.mag,
+                                                  planet.constellation)
+            s += '\t{} and {} ({})\n\n'.format(planet.character,
+                                               planet.constellation.character,
+                                               planet.constellation.element)
+        s += '\n\n'
+        return s
 
 
 LUMINARIES_PARTS = (
@@ -93,9 +161,15 @@ class Luminaries():
         self._parts = [Part(*p) for p in sorted(LUMINARIES_PARTS, key=lambda part: part[0])]
 
     def __getitem__(self, index):
-        print(index)
         if not isinstance(index, int):
             raise Exception('Non-int index: {}'.format(index))
         if 0 > index > len(self._parts):
             raise KeyError('Book doesn\'t have a part number {}'.format(index))
         return self._parts[index-1]
+
+
+if __name__ == "__main__":
+    for part in Luminaries():
+        print(part)
+    import ipdb;ipdb.set_trace()
+    pass
